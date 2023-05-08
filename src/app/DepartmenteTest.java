7af838207eb651b437e3c9e550cca9cfa0044838
Luiz Filipe <luizfilipemr17@gmail.com>
2023-05-08 10:23:52 -0300

    adding DepartmentTest

---
+++ src/app/DepartmenteTest.java
@@ -0,0 +1,7 @@
+package app;
+
+public class DepartmenteTest {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/app/SellerTest.java
+++ src/app/SellerTest.java
@@ -3,39 +3,33 @@ package app;
 import java.util.List;
 
 import model.dao.DaoFactory;
-import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class Main {
+public class SellerTest {
     public static void main(String[] args) throws Exception {
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("##### TEST 1: seller findById #####");
         System.out.println(sellerDao.findById(7));
         System.out.println();
 
-        System.out.println("##### TEST 2: department findById #####");
-        System.out.println(departmentDao.findById(2));
-        System.out.println();
-
-        System.out.println("##### TEST 3: seller findByDepartment");
+        System.out.println("##### TEST 2: seller findByDepartment");
         List<Seller> sellers = sellerDao.findByDepartment(new Department(2, "Electronics"));
         sellers.forEach(System.out::println);
         System.out.println();
 
-        System.out.println("##### TEST 4: seller findAll");
+        System.out.println("##### TEST 3: seller findAll");
         List<Seller> allSellers = sellerDao.findall();
         allSellers.forEach(System.out::println);
         System.out.println();
 
-        System.out.println("##### TEST 5: Seller insert");
+        System.out.println("##### TEST 4: Seller insert");
         // sellerDao.insert(new Seller(null, "Lucas Quadrado", "lucasquadrado@gmail.com", LocalDate.parse("21/02/2000", Seller.fmtBirthDaye), 1210.0, new Department(4, "Books")));
         System.out.println();
 
-        System.out.println("##### TEST 6: Seller Update");
+        System.out.println("##### TEST 5: Seller Update");
         // Seller seller = sellerDao.findById(7);
         // seller.setName("João Pedro");
         // seller.setEmail("jpsoares@gmail.com");
@@ -43,7 +37,7 @@ public class Main {
         // sellerDao.update(seller);
         System.out.println();
 
-        System.out.println("##### TEST 7: Seller Delete #####");
+        System.out.println("##### TEST 6: Seller Delete #####");
         // sellerDao.deleteById(9);
     }
 }
